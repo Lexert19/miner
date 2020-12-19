e34f17b3f6d9bccb0944032effa0b3a57051ac51
lexert19 <dominikch19@gmail.com>
2020-12-19 13:41:04 +0100

    dsfdsfsdf

--- mylagger.js
+++ mylagger.js
@@ -12,7 +12,7 @@ var bot
 let first = false
 function login() {
     bot = mineflayer.createBot({
-        host: "craftplay.pl",
+        host: "192.168.0.123",
         port: 25565,
         version: "1.12.2",
         username: "lagger123",
@@ -107,23 +107,6 @@ async function openChest(){
             cursorY: 0,
             cursorZ: 0, 
         })
-
-        //bot.emit('chestLidMove', target, 0, target)
-        //bot.emit('chestLidMove', target, 0, null)
-        // bot._client.write('block_dig', {
-        //             status: 2, // finish digging
-        //             location: target.position,
-        //             face: 1 // hard coded to always dig from the top
-        //           })
-        // var chest = bot.openChest(target)
-        // var chest = bot.openChest(target)
-        // await new Promise((resolve, reject) => setTimeout(resolve, 50));
-        // chest.close();
-        // await new Promise((resolve, reject) => setTimeout(resolve, 50));
-        // chest.close();
-        // await chest.on('open', function () {
-        //      chest.close();
-        // })
     }catch(e){
 
     }
