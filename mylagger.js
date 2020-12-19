778fdbddb821c623ed393b8fe3ed5ba1b27e75eb
lexert19 <dominikch19@gmail.com>
2020-12-19 13:29:16 +0100

    dsfdsfsdf

--- mylagger.js
+++ mylagger.js
@@ -97,7 +97,7 @@ async function openChest(){
         //console.log("+1");
         //chest = await bot.openChest(target)
 
-        await new Promise((resolve, reject) => setTimeout(resolve, 30));
+        await new Promise((resolve, reject) => setTimeout(resolve, 200));
         bot._client.write('block_place', {
             direction: 1, 
             hand: 0,
